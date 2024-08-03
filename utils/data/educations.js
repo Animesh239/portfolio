bb7b682b35b33c71cd91af28ba1bfdbcfc502190
Animesh239 <animeshbarik239@gmail.com>
2024-08-04 00:13:09 +0530

    dat

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2022 - Present",
+    institution: "National Institute of Technology, Rourkela",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Higher Secondary Education",
+    duration: "2019 - 2021",
+    institution: "ODM Public School",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Secondary School Marticulation",
+    duration: "2009 - 2019",
+    institution: "K.V.No. 1, Sambalpur",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: "FullStack Developer",
+    company: "Fiverr (freelance)",
+    duration: "(Jun 2023 - present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: 'Frontend Developer',
+    company: "Master Tech Services Pvt Ltd.",
+    duration: "(May 2024 - June 2024)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "FullStack Developer",
+    company: "Intellirecruit",
+    duration: "(Sept 2023)"
   }
 ]
