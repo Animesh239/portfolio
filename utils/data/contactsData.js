eb3b2a90d69fe4392fc1e24daa6d57c57298dfba
Animesh239 <animeshbarik239@gmail.com>
2024-08-03 23:34:26 +0530

    contact

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'animeshbarik239@gmail.com',
+    phone: '+91-8260900261',
+    address: 'Sambalpur, Odisha, India',
+    github: 'https://github.com/animesh239',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/in/animesh239/',
+    twitter: 'https://twitter.com/BarikAnimesh239/',
+    stackOverflow: 'https://stackoverflow.com/users/20769696/animesh-barik/',
+    devUsername: "animesh_barik"
 }
