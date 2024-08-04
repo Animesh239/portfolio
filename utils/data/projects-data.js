5c74a863366511020c8d21fcc29d97f3941a43c5
Animesh239 <animeshbarik239@gmail.com>
2024-08-04 11:28:20 +0530

    final

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,47 +6,47 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
+        name: 'Multi-Role Admin Dashboard',
+        description: "Pioneered an interactive dashboard for over 200 users, admins, and superadmins, using Firebase authentication for real-time role-based customization. Displayed analytics with intuitive charts and a global map, improving data comprehension and decision-making.",
+        tools: ['React.Js', 'Firebase', 'Aceternity UI', 'MUI'],
+        role: 'Full Stack Developer',
         code: '',
         demo: '',
         image: crefin,
-        link: 'https://crefin.ai'
+        link: 'https://github.com/Animesh239/Business-Analysis/tree/develop/'
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
+        name: 'NestONDC',
+        description: 'Delivered the ONDC (Open Network for Digital Commerce) site integrated with OpenStreetMap. Businesses and consumers across Bharat will benefit from seamless digital commerce experiences. Leveraging the OpenStreetMap API for location-based services, this platform not only visualizes shop locations on a high-definition geo map and optimizes the efficiency of site performance of the website by 30%.',
+        tools: ['ReactJS', 'Firebase', 'Express.JS'],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
         image: travel,
-        link: 'https://2expedition.com'
+        link: 'https://nestondc.el.r.appspot.com/'
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        name: 'Wordle Game',
+        description: 'Developed a 5-letter word guessing game in React. The game retrieves words from a JSON file, challenging players to guess a random word within five attempts. Implemented a feature that colors the guessed word after each submission, enhancing the gaming experience.',
+        tools: ['React'],
+        role: 'Frontend Developer',
         code: '',
-        role: 'Full Stack Developer',
         demo: '',
         image: realEstate,
-        link: 'https://ayla.ai'
+        link: 'https://game-gta.vercel.app/'
     },
     {
         id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+        name: 'Package-Dekstop',
+        description: "Crafted an Electron.js desktop app with password authentication, CRUD-equipped news dashboard, and news viewer. Revamped robust security protocols, integrated database functionality, and facilitated cross-platform deployment. Promoting efficiency by 30% and achieved 95% user satisfaction.",
+        tools: ['NextJS', 'ElectronJS', 'Firebase'],
+        role: 'Full Stack Developer',
         code: '',
         demo: '',
         image: ayla,
-        role: 'Full Stack Developer',
-        link: 'https://ayla.ai'
+        link: 'https://package-desktop.vercel.app/'
     }
 ];
 
